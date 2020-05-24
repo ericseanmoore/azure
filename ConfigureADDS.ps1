252f88f7050ec21f45554e0bf59cc40d8ab98726
ericseanmoore <65859279+ericseanmoore@users.noreply.github.com>
2020-05-24 09:40:56 -0600

    Add files via upload

---
+++ ConfigureADDS.ps1
@@ -0,0 +1,22 @@
+﻿param($domainName, $netBiosName, $safeModeAdministratorPassword)
+
+$securePassword = ConvertTo-SecureString -String $safeModeAdministratorPassword
+
+#Create Domain Controller
+
+#Install Roles and Features
+Install-WindowsFeature –Name AD-Domain-Services -IncludeManagementTools
+
+Install-ADDSForest `
+-SafeModeAdministratorPassword $securePassword `
+-DomainName $domainName `
+-CreateDnsDelegation:$false ` 
+-DatabasePath "C:\Windows\NTDS" ` 
+-DomainMode "7" ` 
+-DomainNetbiosName $netBiosName ` 
+-ForestMode "7" ` 
+-InstallDns:$true ` 
+-LogPath "C:\Windows\NTDS" ` 
+-NoRebootOnCompletion:$False ` 
+-SysvolPath "C:\Windows\SYSVOL" ` 
+-Force:$true
